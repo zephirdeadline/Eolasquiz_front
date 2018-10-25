da0ba27f1cf7873bd1cc262aceed3811e37d67b1
w4pity <melvyn.petrocchi@epita.fr>
2018-10-25 02:37:34 +0200

    add progressebar

---
+++ src/components/progressbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="ui teal progress" id="example2">
+  <div class="bar"></div>
+  <div class="label">{{progress}}% Done</div>
+</div>
+</template>
+
+<script>
+export default {
+    name:"progressbar",
+    props:["label", "progress"],
+    methods:{
+        udpateProgress(){
+        $("#example2").progress({
+            percent: this.progress
+        });
+        
+            console.log(this.progress)   
+        }
+    },
+    watch:{
+        progress(newVal){
+        this.udpateProgress() 
+        }
+    },
+    mounted(){
+        this.udpateProgress()
+    }
+}
+</script>
+
+<style>
+
+</style>
+
