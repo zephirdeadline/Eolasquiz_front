caa6e811f599b0d90e4f0b8804da88cb7080ee3f
w4pity <melvyn.petrocchi@epita.fr>
2018-10-25 02:34:36 +0200

    update store

---
+++ src/store.js
@@ -0,0 +1,43 @@
+import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
+
+const vuexLocalStorage = new VuexPersist({
+    key: 'vuex', 
+    storage: window.localStorage,
+  })
+
+const state = {
+    user: {},
+    quiz: {},
+    quizConfig: {}
+}
+
+const mutations = {
+    CHANGE_USER: (state, user) => { state.user = user },
+    SET_QUIZ: (state, quiz) => { state.quiz = quiz },
+    SET_QUIZ_CONFIG: (state, config) => { state.quizConfig = config }
+}
+
+const getters = {
+    getUser: state => state.user,
+    getQuiz: state => state.quiz,
+    getQuizConfig: state => state.quizConfig
+}
+
+const actions = {
+    changeUser (store, user) { store.commit('CHANGE_USER', user) },
+    setQuiz (store, quiz) { store.commit('SET_QUIZ', quiz)},
+    setQuizConfig (store, config) {store.commit('SET_QUIZ_CONFIG', config)}
+}
+
+const store = new Vuex.Store({
+    state: state,
+    mutations: mutations,
+    getters: getters,
+    actions: actions,
+    strict: true,
+    plugins: [vuexLocalStorage.plugin]
+})
+
+
+export default store
