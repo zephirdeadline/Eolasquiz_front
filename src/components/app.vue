7b61be48a9bbe7ae26b7357657ffa57bc4d4d60c
w4pity <melvyn.petrocchi@epita.fr>
2018-10-25 02:36:58 +0200

    add app sementic ui

---
+++ src/components/app.vue
@@ -0,0 +1,31 @@
+
+<template>  
+    <div>
+        <div class="ui secondary pointing menu" >
+            <router-link :to="{ name: 'welcome'}" class="item" exact>
+                Home
+            </router-link>
+            <router-link :to="{ name: 'admin'}" class="item" exact>
+                Admin
+            </router-link>
+           
+            <div class="right menu">
+                <a class="ui item">
+                Logout
+                </a>
+            </div>
+            </div>
+            <div class="ui segment">
+              <router-view></router-view>
+            </div>
+
+    </div>
+    
+</template>
+
+
+<script>
+export default { 
+  store: require('../store').default,
+}
+</script>
