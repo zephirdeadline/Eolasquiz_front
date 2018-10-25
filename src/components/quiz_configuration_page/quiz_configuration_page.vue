46cebfe8ec2a129c5989cbaa7c30de7dfbca7e82
w4pity <melvyn.petrocchi@epita.fr>
2018-10-25 02:36:03 +0200

    add configurtation page

---
+++ src/components/quiz_configuration_page/quiz_configuration_page.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="quiz_config">
+        <div class="ui card ">
+            <div class="image"> 
+                <img src="../../assets/quiz.jpg">
+            </div>
+            <div class="content">
+                <a class="header">{{quiz.name}}</a>
+                <div class="meta">
+                <span class="date">{{quiz.created_at}}</span>
+                </div>
+                <div class="description">
+                    {{ quiz.description }}
+                </div>
+            </div>
+            <div class="extra content">
+                <!-- <a>
+                <i class="like icon" @click="Like(quiz.id)"></i>
+                {{ quiz.likes.length }} <span v-if="isUserLikeQuiz(quiz)">with you</span>
+                </a>
+                <a>
+                <i class="thumbs down icon"  @click="Dislike(quiz.id)"></i>
+                {{ quiz.dislikes.length }} <span v-if="isUserDislikeQuiz(quiz)">with you</span>
+                </a> -->
+            </div>
+        </div>
+
+        <form class="ui form" @submit.prevent>
+        <div class="field">
+            <label for="">Nombre de questions</label>
+            <input name="nb_questions" type="number" v-model="nb_question" placeholder="10"/>
+        </div>
+        
+        <input class="ui button" type="submit" value="Tester!" @click="runtest()">
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'quiz',
+    data () {
+        return {
+            quiz: {},
+            nb_question: 10,
+
+            headers:{ headers: {Authorization: this.$store.getters.getUser.token}}
+        }
+    },
+
+    methods:{
+        getQuizFromServer(id) {
+             this.$http.get('api/quiz/' + id, this.headers).then(Response => { this.quiz = JSON.parse(Response.bodyText); this.$store.dispatch('setQuiz', this.quiz) }, console.log(Response))
+        },
+
+        runtest() {
+            var config_quiz = {"nb_question": this.nb_question}
+
+            this.$store.dispatch('setQuizConfig', config_quiz)
+            this.$router.push("/test")
+        }
+    },
+    
+    mounted() {
+         this.getQuizFromServer(this.$route.params.id)
+    }
+}
+</script>
+
+
+<style>
+.quiz_config {
+    margin: auto;
+    text-align: center;
+    max-width: 500px;
+}
+
+</style>
