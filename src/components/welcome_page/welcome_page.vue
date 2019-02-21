be6605191665afd1c0c72c2fb540d4535cf1042d
Melvyn Petrocchi <mpetrocchi@ennov.com>
2019-02-21 16:35:43 +0100

    add search bar

--- src/components/welcome_page/welcome_page.vue
+++ src/components/welcome_page/welcome_page.vue
@@ -1,126 +1,149 @@
 <template>
-<div class="content">
-
+  <div class="content">
     <h1 class="ui center aligned icon header title">
-        <i class="circular users icon"></i>
-        Kaestio
-       </h1>
+      <i class="circular users icon"></i>
+    </h1>
+    <div class="ui center aligned icon input">
+      <h1>Kaestio →</h1>
+      <form @submit="FindQuiz" method="GET" >
+        <input v-model="toFind" name="value" type="text" placeholder="Search...">
+        <i class="circular search link icon"></i>
+    </form>
+      
+    </div>
     <div>
-        <div class="ui segment" id="loader" v-if="is_loaded">
-            <div class="ui active inverted dimmer">
-                <div class="ui text loader">Loading</div>
-            </div>
+      <div class="ui segment" id="loader" v-if="is_loaded">
+        <div class="ui active inverted dimmer">
+          <div class="ui text loader">Loading</div>
         </div>
+      </div>
     </div>
     <hr>
     <div class="ui four quiz stackable cards" v-if="!is_loaded">
-        <div class="ui card " id="card" v-for="quiz in quizs" :key="quiz.id">
-            <div class="image"> 
-                <img src="../../assets/quiz.jpg">
-            </div>
-            <div class="content">
-                <router-link :to="{ name: 'quiz', params: { id: quiz.id }}" class="header">{{quiz.name}}</router-link>
-                <div class="meta">
-                <span class="date">{{quiz.created_at}}</span>
-                </div>
-                <div class="description">
-                    {{ quiz.description }}
-                </div>
-            </div>
-            <div class="extra content">
-                <a>
-                <i class="like icon" @click="Like(quiz.id)"></i>
-                {{ quiz.likes.length }} <span v-if="isUserLikeQuiz(quiz)">with you</span>
-                </a>
-                <a>
-                <i class="thumbs down icon"  @click="Dislike(quiz.id)"></i>
-                {{ quiz.dislikes.length }} <span v-if="isUserDislikeQuiz(quiz)">with you</span>
-                </a>
-                
-            </div>
-
-             
-    
+      <div class="ui card" id="card" v-for="quiz in quizs" :key="quiz.id">
+        <div class="image">
+          <img src="../../assets/quiz.jpg">
         </div>
+        <div class="content">
+          <router-link :to="{ name: 'quiz', params: { id: quiz.id }}" class="header">{{quiz.name}}</router-link>
+          <div class="meta">
+            <span class="date">{{quiz.created_at}}</span>
+          </div>
+          <div class="description">{{ quiz.description }}</div>
+        </div>
+        <div class="extra content">
+          <a>
+            <i class="like icon" @click="Like(quiz.id)"></i>
+            {{ quiz.likes.length }}
+            <span v-if="isUserLikeQuiz(quiz)">with you</span>
+          </a>
+          <a>
+            <i class="thumbs down icon" @click="Dislike(quiz.id)"></i>
+            {{ quiz.dislikes.length }}
+            <span v-if="isUserDislikeQuiz(quiz)">with you</span>
+          </a>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 
 <script>
-import Vuex from 'vuex'
+import Vuex from "vuex";
 
 export default {
-    name: "welcome",
+  name: "welcome",
 
-    data () {
-        return {
-            quizs: [],
-            headers:{ headers: {Authorization: this.$store.getters.getUser.token}},
-            is_loaded: true
-        }
-    },
-    
-    computed: {
-       
-    },
+  data() {
+    return {
+      toFind: "",
+      quizs: [],
+      headers: {
+        headers: { Authorization: this.$store.getters.getUser.token }
+      },
+      is_loaded: true
+    };
+  },
 
-    methods: {
-        Like (id) {
-            this.$http.post('api/like/', [{ "quiz": id }], this.headers).then(this.getQuizs, Response => Response)
-        },
-        Dislike (id) {
-            this.$http.post('api/dislike/', [{ quiz: id }], this.headers).then(this.getQuizs, Response => Response)
-        },
-        isUserLikeQuiz (quiz) {
-            return quiz.likes.find(like => like.user === this.$store.getters.getUser.id )
-        },
-        isUserDislikeQuiz (quiz) {
-            return quiz.dislikes.find(like => like.user === this.$store.getters.getUser.id )
-        },
-        getQuizs () {
-            this.$http.get('api/quiz/', this.headers).then( 
-            Response => { 
-                this.quizs = JSON.parse(Response.bodyText) 
-                this.is_loaded = false
-                }, 
-            Response => 
-                console.log(Response))
-        }
-    },
+  computed: {},
 
-    mounted () {
+  methods: {
+    FindQuiz(e) {
+      this.is_loaded = true
+      if(this.toFind === ""){
         this.getQuizs()
+        return
+      }
+      this.$http.get("api/quiz/find/" + this.toFind).then(
+        Response => {
+          this.quizs = JSON.parse(Response.bodyText);
+          this.is_loaded = false;
+        },
+        Response => console.log(Response.error)
 
+      );
+      e.preventDefault();
+    },
+    Like(id) {
+      this.$http
+        .post("api/like/", [{ quiz: id }], this.headers)
+        .then(this.getQuizs, Response => Response);
+    },
+    Dislike(id) {
+      this.$http
+        .post("api/dislike/", [{ quiz: id }], this.headers)
+        .then(this.getQuizs, Response => Response);
+    },
+    isUserLikeQuiz(quiz) {
+      return quiz.likes.find(
+        like => like.user === this.$store.getters.getUser.id
+      );
+    },
+    isUserDislikeQuiz(quiz) {
+      return quiz.dislikes.find(
+        like => like.user === this.$store.getters.getUser.id
+      );
+    },
+    getQuizs() {
+      this.$http.get("api/quiz/", this.headers).then(
+        Response => {
+          this.quizs = JSON.parse(Response.bodyText);
+          this.is_loaded = false;
+        },
+        Response => console.log(Response)
+      );
     }
-}
+  },
+
+  mounted() {
+    this.getQuizs();
+  }
+};
 </script>
 
 
 <style>
-
 .lazyloaded {
-    width: 100% !important; 
-    height: 100% !important; 
+  width: 100% !important;
+  height: 100% !important;
 }
 
-.thumbs.down.icon
-{
-    margin-left: 5px
+.thumbs.down.icon {
+  margin-left: 5px;
 }
 .quiz {
-    display: flex;
-   height: 100%;
+  display: flex;
+  height: 100%;
 }
 
 h1 {
-    text-align: center
+  text-align: center;
 }
 #loader {
-    height: 500px;
+  height: 500px;
 }
 .title {
-    color: aliceblue !important
+  color: aliceblue !important;
 }
-
 </style>
